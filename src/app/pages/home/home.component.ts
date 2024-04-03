85adca3f7e606e38c6ab64f88aaa1d39ec35e1e0
joao <jap.la@hotmail.com>
2024-04-03 17:49:27 -0400

    add filter input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,29 +2,60 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../../services/api.service';
-import { map, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, CommonModule, FormsModule],
+  imports: [CardComponent, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   countries: any[] = [];
-  searchTerm: string = '';
   countries$ = new Observable<any[]>();
+  searchFilter: FormGroup;
+  filterValue: string;
 
   constructor(public apiService: ApiService) {}
 
   ngOnInit(): void {
     this.loadCountries();
+    this.createFormFilter();
+    this.createFilter();
   }
 
   loadCountries() {
     this.countries$ = this.apiService.getCountries();
+  }
+
+  createFormFilter() {
+    this.searchFilter = new FormGroup({
+      filterInput: new FormControl(''),
+    });
+  }
+
+  createFilter() {
+    this.searchFilter?.controls['filterInput']?.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((value: string) => {
+        if (value.trim() === '') {
+          this.loadCountries();
+        } else {
+          this.filterValue = value;
+          this.filterCountries();
+        }
+      });
+  }
+
+  filterCountries() {
+    this.countries$ = this.apiService.filterCountry(this.filterValue);
   }
 }
