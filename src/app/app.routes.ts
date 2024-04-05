780a2ec3a25876a2b33c7bd3063510bfbe0c7d00
joao <jap.la@hotmail.com>
2024-04-05 17:21:55 -0400

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { CountryDetailsComponent } from './pages/country-details/country-details.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'details/:id', component: CountryDetailsComponent },
-
-];
